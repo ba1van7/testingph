ab34c8367e6e26931d66e82534d36b498fd1ee21
Ba1van7 <102735124+ba1van7@users.noreply.github.com>
2023-12-05 15:01:27 +0530

    Update save.php

--- save.php
+++ save.php
@@ -1,10 +1,16 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$file = fopen("data.txt", "a") or die("some error occurred"); // open file, create if doesn't exist
-$txt = "$username $password\n"; // store data as username password
-fwrite($file, $txt); // write to our file
-fclose($file); // close the file handle
+    $file = fopen("data.txt", "a") or die("Unable to open file"); // open file, create if doesn't exist
+    $txt = "$username $password\n"; // store data as username password
+    fwrite($file, $txt); // write to our file
+    fclose($file); // close the file handle
 
-echo 'Your username and password are stored in our database. Change it soon!';
+    echo 'Your username and password are stored in our database. Change it soon!';
+} else {
+    // Handle other HTTP methods or direct access to the file
+    http_response_code(405);
+    echo 'Method Not Allowed';
+}
